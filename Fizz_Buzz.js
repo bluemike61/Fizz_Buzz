f954f232775d7111d420b4548e1a7d7333ac73b4
Michael Janus <46409972+bluemike61@users.noreply.github.com>
2022-01-14 14:41:42 -0500

    added buzz case

--- Fizz_Buzz.js
+++ Fizz_Buzz.js
@@ -3,6 +3,9 @@ function fizzbuzzFunction(x) {
         if (i%3 == 0 && i%5 == 0 && i != 0) {
             console.log('Fizz Buzz');
         }
+        else if (i%5 == 0 && i != 0){
+            console.log('Buzz');
+        }
         else if (i%3== 0 && i != 0){
             console.log('Fizz');
         }
